2f5b77dd8c0aeaeadce8a415e6fe457332db5784
KakoDev <claudio.jara@me.com>
2016-07-15 13:21:18 -0400

    second test

--- GitApp/ViewController.swift
+++ GitApp/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("Hello World")
+        
+        print("bye world!")
     }
 
     override func didReceiveMemoryWarning() {
